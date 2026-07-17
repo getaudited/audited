b83f13438553f3a9336247d51950ce341c639415
Firmino Changani <firmino.changani@gmail.com>
2026-07-17 14:49:46 +0100

    test event types repo methods

--- internal/adapters/clickhouse/event_types_test.go
+++ internal/adapters/clickhouse/event_types_test.go
@@ -6,9 +6,11 @@ import (
 	"time"
 
 	"github.com/brianvoe/gofakeit/v7"
-	chadapters "github.com/getaudited/audited/internal/adapters/clickhouse"
-	"github.com/getaudited/audited/internal/domain"
 	"github.com/stretchr/testify/require"
+
+	chadapters "github.com/getaudited/audited/internal/adapters/clickhouse"
+	"github.com/getaudited/audited/internal/app/query"
+	"github.com/getaudited/audited/internal/domain"
 )
 
 func TestEventTypes_FindByAction(t *testing.T) {
@@ -16,7 +18,7 @@ func TestEventTypes_FindByAction(t *testing.T) {
 		t.Parallel()
 
 		repo := chadapters.NewEventTypesClickhouseRepository(db)
-		eventTypes := seedEventTypes(t, repo, 1)
+		eventTypes, _ := seedEventTypes(t, repo, 1)
 		eventType := eventTypes[0]
 
 		found, err := repo.FindByAction(ctx, eventType.Action)
@@ -46,15 +48,97 @@ func TestEventTypes_FindByAction(t *testing.T) {
 		require.ErrorAs(t, err, &errMockedClickhouse)
 	})
 }
-func TestEventTypes_QueryAll(t *testing.T)            {}
-func TestEventTypes_Delete(t *testing.T)              {}
+
+func TestEventTypes_QueryAll(t *testing.T) {
+	t.Run("query_all", func(t *testing.T) {
+		t.Parallel()
+
+		repo := chadapters.NewEventTypesClickhouseRepository(db)
+		seedCount := 10
+		_, eventTypes := seedEventTypes(t, repo, seedCount)
+
+		result, err := repo.QueryAll(ctx, query.AllEventTypes{
+			PaginationParams: query.PaginationParams{
+				Limit: 20,
+				Page:  1,
+			},
+		})
+		require.NoError(t, err)
+		require.GreaterOrEqual(t, len(result.Data), seedCount)
+
+		var totalFound int
+		for _, eventType := range result.Data {
+			foundEventType, found := eventTypes[eventType.Action]
+			if found {
+				totalFound++
+				requireEqualEventTypes(t, foundEventType, eventType)
+			}
+		}
+
+		require.Equal(t, seedCount, totalFound)
+	})
+
+	t.Run("query_all_filter_by_action", func(t *testing.T) {
+		t.Parallel()
+
+		repo := chadapters.NewEventTypesClickhouseRepository(db)
+		eventTypes, _ := seedEventTypes(t, repo, 1)
+
+		result, err := repo.QueryAll(ctx, query.AllEventTypes{
+			Action: new(eventTypes[0].Action),
+			PaginationParams: query.PaginationParams{
+				Limit: 1,
+				Page:  1,
+			},
+		})
+		require.NoError(t, err)
+		require.Len(t, result.Data, 1)
+		requireEqualEventTypes(t, eventTypes[0], result.Data[0])
+	})
+
+	t.Run("error_querying", func(t *testing.T) {
+		t.Parallel()
+
+		repo := chadapters.NewEventTypesClickhouseRepository(dbError)
+		_, err := repo.QueryAll(ctx, query.AllEventTypes{})
+		require.ErrorAs(t, err, &errMockedClickhouse)
+	})
+}
+
+func TestEventTypes_Delete(t *testing.T) {
+	t.Run("delete_event_type", func(t *testing.T) {
+		t.Parallel()
+
+		repo := chadapters.NewEventTypesClickhouseRepository(db)
+		eventTypes, _ := seedEventTypes(t, repo, 1)
+
+		err := repo.Delete(ctx, eventTypes[0].Action)
+		require.NoError(t, err)
+
+		_, err = repo.FindByAction(ctx, eventTypes[0].Action)
+		require.ErrorIs(t, err, domain.ErrEventTypeNotFound)
+	})
+
+	t.Run("error_deleting", func(t *testing.T) {
+		repo := chadapters.NewEventTypesClickhouseRepository(dbError)
+		err := repo.Delete(ctx, "some-action")
+		require.ErrorAs(t, err, &errMockedClickhouse)
+	})
+}
+
 func TestEventTypes_Save(t *testing.T)                {}
 func TestEventTypes_RollbackVersion(t *testing.T)     {}
 func TestEventTypes_SaveVersion(t *testing.T)         {}
 func TestEventTypes_AllVersionsByAction(t *testing.T) {}
 
-func seedEventTypes(t *testing.T, repo *chadapters.EventTypesClickhouseRepository, count int) []domain.EventType {
+func seedEventTypes(
+	t *testing.T,
+	repo *chadapters.EventTypesClickhouseRepository,
+	count int,
+) ([]domain.EventType, map[string]domain.EventType) {
 	eventTypes := make([]domain.EventType, count)
+	eventTypesByAction := map[string]domain.EventType{}
+
 	for i := 0; i < count; i++ {
 		eventType := domain.EventType{
 			Action:                       fmt.Sprintf("test.created.%s", domain.NewID()),
@@ -74,7 +158,19 @@ func seedEventTypes(t *testing.T, repo *chadapters.EventTypesClickhouseRepositor
 		require.NoError(t, err)
 
 		eventTypes[i] = eventType
+		eventTypesByAction[eventType.Action] = eventType
 	}
 
-	return eventTypes
+	return eventTypes, eventTypesByAction
+}
+
+func requireEqualEventTypes(t *testing.T, expected domain.EventType, got query.EventType) {
+	t.Helper()
+
+	require.Equal(t, expected.Action, got.Action)
+	require.Equal(t, expected.TargetTypes, got.TargetTypes)
+	require.Equal(t, expected.Version, got.Version)
+	require.Equal(t, string(expected.Schema), got.Schema)
+	require.Equal(t, expected.ShouldValidateMetadataSchema, got.ShouldValidateMetadataSchema)
+	require.WithinDuration(t, expected.CreatedAt, got.CreatedAt, time.Second)
 }
