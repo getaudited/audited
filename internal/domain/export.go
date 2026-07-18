e33efd6ad4fcd5e3aa6dc6d87a7c8d27382907e3
Firmino Changani <firmino.changani@gmail.com>
2026-07-18 19:47:52 +0100

    test domain/id

--- internal/domain/export.go
+++ internal/domain/export.go
@@ -1,15 +1,13 @@
 package domain
 
-import "time"
-
-type Export struct {
-	ID        string
-	State     string
-	Url       string
-	Targets   []string
-	Actors    []string
-	Actions   []string
-	RangeFrom time.Time
-	RangeTo   time.Time
-	CreatedAt time.Time
-}
+//type Export struct {
+//	ID        string
+//	State     string
+//	Url       string
+//	Targets   []string
+//	Actors    []string
+//	Actions   []string
+//	RangeFrom time.Time
+//	RangeTo   time.Time
+//	CreatedAt time.Time
+//}
